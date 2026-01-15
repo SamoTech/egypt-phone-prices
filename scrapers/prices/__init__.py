ebd3c1c99cc61645b035f5b6e4caeffc561b7b54
Ossama Hashim <samo.hossam@gmail.com>
2026-01-15 19:17:12 +0200

    Update __init__.py to register new scrapers

--- scrapers/prices/__init__.py
+++ scrapers/prices/__init__.py
@@ -1,14 +1,1 @@
-"""
-Price Scrapers Package
-Intelligent price scraping using Playwright for Egyptian marketplaces.
-"""
-
-from .amazon import AmazonEgPriceScraper
-from .jumia import JumiaEgPriceScraper
-from .noon import NoonEgPriceScraper
-
-__all__ = [
-    'AmazonEgPriceScraper',
-    'JumiaEgPriceScraper',
-    'NoonEgPriceScraper',
-]
+from .btech import BTechScraper\nfrom .dream2000 import Dream2000Scraper\nfrom .dubai_phone import DubaiPhoneScraper\n\n__all__ = ['BTechScraper', 'Dream2000Scraper', 'DubaiPhoneScraper']
